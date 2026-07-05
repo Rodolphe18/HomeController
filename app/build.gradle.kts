3d21b9f6e7300064ba3dadd2a88f5f35c60746f3
rodolphe <rodolphefrancotte18@gmail.com>
2026-07-05 19:15:55 +0200

    build: passe compileSdk à 37 (requis par lifecycle-*-compose 2.11.0)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,9 +6,7 @@ plugins {
 android {
     namespace = "com.francotte.homecontroller"
     compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
+        version = release(37)
     }
 
     defaultConfig {
