439075a85d2800aa7f7ea95f0f9705999ff8c2a7
rodolphe <rodolphefrancotte18@gmail.com>
2026-07-09 15:59:30 +0200

    BtClassicScanException created

---
+++ core/bluetooth/src/main/java/com/francotte/homecontroller/core/bluetooth/BtClassicScanException.kt
@@ -0,0 +1,4 @@
+package com.francotte.homecontroller.core.bluetooth
+
+/** Erreur de découverte Bluetooth Classic (adaptateur indisponible, démarrage impossible). */
+class BtClassicScanException(message: String) : Exception(message)
