1c4fb041a8fe1f63129794ff5957c92bd288c7a1
rodolphe <rodolphefrancotte18@gmail.com>
2026-07-09 15:58:03 +0200

    drawables updated with phosphore assets

---
+++ core/designsystem/src/main/java/com/francotte/homecontroller/core/designsystem/AppIcons.kt
@@ -0,0 +1,11 @@
+package com.francotte.homecontroller.core.designsystem
+
+import androidx.annotation.DrawableRes
+
+/** Icônes de l'app (Phosphor, importées en vector drawables). */
+object AppIcons {
+    @DrawableRes val HomeAssistant: Int = R.drawable.ic_home_assistant
+    @DrawableRes val Ble: Int = R.drawable.ic_ble
+    @DrawableRes val BtClassic: Int = R.drawable.ic_bt_classic
+    @DrawableRes val Settings: Int = R.drawable.ic_settings
+}
